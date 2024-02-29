99007ec35c274e44d2f6ccb669f889ae6eaf2127
CrispStrobe <154636388+CrispStrobe@users.noreply.github.com>
2024-02-29 12:14:51 +0100

    +poe util.py

--- lib/util.py
+++ lib/util.py
@@ -108,8 +108,8 @@ def parse_batch(batch, ooba_launch_script, launch_ooba):
 			
 			# Read inference engine option from config
 			inference_engine = inference_engine.strip().lower()
-			if inference_engine not in ['transformers', 'ooba', 'openai']:
-				raise Exception("inference_engine in config.cfg must be transformers, openai or oobabooga.")
+			if inference_engine not in ['transformers', 'ooba', 'openai', 'poe']:
+				raise Exception("inference_engine in config.cfg must be transformers, openai, poe or oobabooga.")
 			if inference_engine == 'ooba' and not ooba_launch_script:
 				raise Exception('ooba_launch_script not set in config.cfg')
 			
